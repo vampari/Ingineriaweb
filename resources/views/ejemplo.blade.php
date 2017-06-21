9d810a551ae970193962d62d012d620afb55ddff
Vampari <soma_wtf@hotmail.com>
2017-06-21 08:30:31 -0500

    Dia 2 ing web

---
+++ resources/views/ejemplo.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Vista de ejemplo</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row well">
+        <h1>Ejemplo de view</h1>
+        <h2>Bienvenida, {{$nombre}}</h2>
+        <h2>Edad: {{$edad}}</h2>
+      </div>
+
+    </div>
+
+  </body>
+</html>
